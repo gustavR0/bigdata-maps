371f9fcad574e5c489d02217d156acba9468c4ca
gustavR0 <gust040825@gmail.com>
2020-11-17 09:47:22 -0600

    asignacion de puerto

--- src/app/services/data-api.service.ts
+++ src/app/services/data-api.service.ts
@@ -16,7 +16,7 @@ export class DataApiService {
  
 
   // apiURL = 'http://localhost:8080/api/auth/';
-  apiURL = 'http://104.198.244.0:5028/api/auth/';
+  apiURL = 'http://104.198.244.0:5051/api/auth/';
   //apiURL = 'https://valid-decoder-258800.appspot.com/';
   
   
